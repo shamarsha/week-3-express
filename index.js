a8e1555b719bb140993c5dbf65b796cfbc8b6299
shamarsha <marshakamila8@gmail.com>
2023-12-01 22:23:46 +0700

    first commit

--- index.js
+++ index.js
@@ -4,10 +4,8 @@ const app = express()
 var cors = require('cors')
 var bodyParser = require('body-parser')
 
-// create application/json parser
 var jsonParser = bodyParser.json()
 
-// create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 const port = 3000
@@ -17,30 +15,17 @@ app.use(jsonParser);
 app.use(urlencodedParser);
 
 app.get('/', (req, res) => {
-  res.json('Hello World!')
+    res.json('website A')
 })
 
-app.post('/login', (req, res) => {
-    // Simulasi data dari database
-    const username = 'joko'
-    const password = '12345678'
+app.post('/github-event', (req, res) => {
+  if (req.body.secret !== 'secret123') {
+    return res.status(403).json({ error: "Invalid secret" });
+  }
 
-    if (req.body.username === username) {
-        if (req.body.password === password) {
-            res.json({
-                status: 'success'
-            })
-        }  else {
-            res.json({
-                status: 'error, wrong password'
-            })
-        }
-    } else {
-        res.json({
-            status: 'error, username not found'
-        })
-    }
-  })
+  console.log('Incoming Webhook')
+  res.json("")
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
